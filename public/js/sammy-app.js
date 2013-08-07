20b436b721990dab2e3b4418fdf30337a96ad67e
David Vogeleer <david@individual11.com>
2013-08-07 17:44:34 -0400

    fixed some animation speed and hopefully the hash issues

--- public/js/sammy-app.js
+++ public/js/sammy-app.js
@@ -1,5 +1,5 @@
 var app = Sammy('#main', function(){
-	this.get(Colors.core.root + '/#/track/:trackNumber', function(context) {
+	this.get(Colors.core.root + '#/track/:trackNumber', function(context) {
 		var currentPosition = Number(this.params['trackNumber']);
 		if((isDesktop && isFullScreen || isTouch) && currentPosition != NaN){
 	        var currentPrev = (Colors.position > 1)? Colors.position-1:Colors.length,
